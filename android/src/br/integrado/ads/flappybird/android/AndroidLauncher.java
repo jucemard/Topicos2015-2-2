4fa82e78ffffd8a545fcdc3a5cf0d45e693c6f97
Jucemar Dias da Silva <jucemar-bmb@hotmail.com>
2015-09-28 20:00:22 -0300

    commit inicial

---
+++ android/src/br/integrado/ads/flappybird/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.integrado.ads.flappybird.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.integrado.ads.flappybird.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ desktop/src/br/integrado/ads/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.integrado.ads.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.integrado.ads.flappybird.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
